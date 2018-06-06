237a4f4c7a03bedc40276817e9787e27ff2f6de0
袁舰 <30914536@qq.com>
2018-06-06 18:36:26 +0800

    积木块
    Input指令练习
    0，语音指令，随机生成积木块（完成）
    1，聚焦积木，边框高亮显示（完成）
    2，单手，拖动积木，双手旋转积木（完成）
    3，单手，推到积木（完成）
    4，语音指令，移除聚焦积木（完成）
    5，语音指令，清除所有积木（完成）

---
+++ play/script/BuildBlock.cs
@@ -0,0 +1,59 @@
+﻿using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.UX;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace yuanjian.Block
+{ 
+    public class BuildBlock : MonoBehaviour {
+
+        public List<GameObject> objs;
+
+        private Color[] colors = { Color.blue, Color.yellow, Color.red, Color.green, Color.magenta, Color.grey, Color.black, Color.white };
+
+        // Use this for initialization
+        void Start () {
+      
+        }
+	
+	    // Update is called once per frame
+	    void Update () {
+		
+	    }
+
+        public void Build()
+        {
+            if (CameraCache.Main.transform != null)
+            {
+                Vector3 position = CameraCache.Main.transform.position;
+                position.Set(position.x, position.y + 2, position.z);
+                int i = Random.Range(0, 3);
+                GameObject obj;
+                obj = Instantiate(objs[i], position + CameraCache.Main.transform.forward * 4, Quaternion.identity);
+                obj.GetComponent<Renderer>().material.color = colors[Random.Range(0, 8)];
+            }
+        }
+
+        public void Remove()
+        {
+            IPointingSource pointingSource = null;
+            if(FocusManager.Instance.TryGetSinglePointer(out pointingSource))
+            {
+                GameObject obj = FocusManager.Instance.GetFocusedObject(pointingSource);
+                obj.GetComponent<MoveBlock>().ShowBoundingBox = false;
+                Destroy(obj);
+            }
+        }
+
+        public void Clear()
+        {
+            GameObject[] objs = GameObject.FindGameObjectsWithTag("block");
+            foreach(GameObject obj in objs)
+            {
+                obj.GetComponent<MoveBlock>().ShowBoundingBox = false;
+                Destroy(obj);
+            }
+        }
+    }
+}
